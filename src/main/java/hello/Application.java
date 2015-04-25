6684553d0e4dc4cf5147e397f3467274eaf6fe69
xtfkpi <xtfkpi@xtfkpi-Lenovo-E10-30>
2015-04-25 12:26:57 +0300

    it calculate distance

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,6 +1,12 @@
 package hello;
 
 //import org.json.simple.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -11,6 +17,12 @@ public class Application {
     	String json = "{\"paramsArray\": [\"first\", 100],"
                 + "\"paramsObj\": {\"one\": \"two\", \"three\": \"four\"},"
                 + "\"paramsStr\": \"some string\"}";
+    	String url="";
+    	try {
+			json=getContentOfHTTPPage("");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
     	System.out.println("json- "+json);
 
     JSONParser parser = new JSONParser();
@@ -24,18 +36,45 @@ public class Application {
 		e.printStackTrace();
 	}
     JSONObject jsonObj = (JSONObject) obj;
-    System.out.println(jsonObj.get("paramsArray"));
-    // some string
+    System.out.println(jsonObj);
+//    System.out.println(jsonObj.get("paramsObj"));
+//
+//    // some string
+    JSONArray ja = (JSONArray) jsonObj.get("routes");
+    System.out.println(ja.get(0));
+    jsonObj=(JSONObject) ja.get(0);
+    System.out.println(jsonObj.get("legs"));
+    ja=(JSONArray) jsonObj.get("legs");
+    System.out.println(ja.get(0));
+    jsonObj=(JSONObject) ja.get(0);
+    System.out.println(jsonObj.get("distance"));
+    jsonObj=(JSONObject) jsonObj.get("distance");
+    System.out.println(jsonObj.get("value"));
+    //ja=(JSONArray) jsonObj.get(0);
+    //ja = (JSONArray) ja.get(0);
 
 //    JsonObject jo = jsonObj.get("paramsObj");
 //    System.out.println(jo.get("three"));
 //    // four
 //
-//    JsonArray ja = jsonObj.get("paramsArray");
-//    System.out.println(ja.get(1));
+
 //    // 100
-
-
     }
+    public static String getContentOfHTTPPage(String pageAddress) throws Exception {
+    	StringBuilder sb = new StringBuilder();
+    	URL pageURL = new URL("http://maps.googleapis.com/maps/api/directions/"
+    			+ "json?origin=%D0%9A%D0%B8%D0%B5%D0%B2,%20%D0%9A%D1%80%D0%B5%D1%89%D0%B0%D1%82%D0%B8%D0%BA%201"
+    			+ "&destination=%D0%9A%D0%B8%D0%B5%D0%B2,%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%20%D0%BF%D0%BE%D0%B1%D0%B5%D0%B4%D1%8B%2022"
+    			+ "&region=ua&sensor=false"); 
+    	URLConnection uc = pageURL.openConnection();
+    	BufferedReader br = new BufferedReader( new InputStreamReader( uc.getInputStream()));
+    	try {
+    		String inputLine; 
+    		while ((inputLine = br.readLine()) != null) {
+    			sb.append(inputLine); 
+    		}
+    		} 
+    	finally { br.close(); } 
+    	return sb.toString(); }
 
 }
